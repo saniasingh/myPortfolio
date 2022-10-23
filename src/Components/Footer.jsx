8cbdea59d4967a08473b6b8f7f23ccdedcec3342
Sania Singh <saniasingh4in@gmail.com>
2022-10-23 14:22:08 +0530

    some fixes

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 function Footer() {
 
-const  currentDate = new Date().toJSON().slice(0, 10);
+const  currentDate = new Date().getFullYear().toString();
 
 
     return (
     <div className="footer">
-        <h3>Made with❤️by Sania Singh</h3>
+        <h6>Made with ❤️ by Sania Singh</h6>
          <p>©️{currentDate}</p>
 
     </div>
